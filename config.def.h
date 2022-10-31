b5b7275226696bb8f55f0bdd5df9c2c15f2944cd
versorspace <resoaxes@gmail.com>
2022-10-31 11:55:48 +0530

    code indenting for readability

--- config.def.h
+++ config.def.h
@@ -35,27 +35,27 @@ static Bool npisrelative  = False;
 
 #define MODKEY ControlMask
 static Key keys[] = {
-	/* modifier             key        function     argument */
-	{ MODKEY|ShiftMask,     XK_n, 	   focusonce,   { 0 } },
-	{ MODKEY|ShiftMask,     XK_n, 	   spawn,       { 0 } },
+	/* modifier             key        	function     argument */
+	{ MODKEY|ShiftMask,     XK_n, 	   	focusonce,   { 0 } },
+	{ MODKEY|ShiftMask,     XK_n, 	   	spawn,       { 0 } },
 
-	{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
-	{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_j,      movetab,     { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
-	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
+	{ MODKEY|ShiftMask,     XK_l,      	rotate,      { .i = +1 } },
+	{ MODKEY|ShiftMask,     XK_h,      	rotate,      { .i = -1 } },
+	{ MODKEY|ShiftMask,     XK_bracketleft, movetab,     { .i = -1 } },
+	{ MODKEY|ShiftMask,     XK_bracketright,movetab,     { .i = +1 } },
+	{ MODKEY,               XK_Tab,    	rotate,      { .i = 0 } },
 
-	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
-	{ MODKEY,               XK_1,      move,        { .i = 0 } },
-	{ MODKEY,               XK_2,      move,        { .i = 1 } },
-	{ MODKEY,               XK_3,      move,        { .i = 2 } },
-	{ MODKEY,               XK_4,      move,        { .i = 3 } },
-	{ MODKEY,               XK_5,      move,        { .i = 4 } },
-	{ MODKEY,               XK_6,      move,        { .i = 5 } },
-	{ MODKEY,               XK_7,      move,        { .i = 6 } },
-	{ MODKEY,               XK_8,      move,        { .i = 7 } },
-	{ MODKEY,               XK_9,      move,        { .i = 8 } },
-	{ MODKEY,               XK_0,      move,        { .i = 9 } },
+	{ MODKEY,               XK_grave,  	spawn,       SETPROP("_TABBED_SELECT_TAB") },
+	{ MODKEY,               XK_1,      	move,        { .i = 0 } },
+	{ MODKEY,               XK_2,      	move,        { .i = 1 } },
+	{ MODKEY,               XK_3,      	move,        { .i = 2 } },
+	{ MODKEY,               XK_4,      	move,        { .i = 3 } },
+	{ MODKEY,               XK_5,      	move,        { .i = 4 } },
+	{ MODKEY,               XK_6,      	move,        { .i = 5 } },
+	{ MODKEY,               XK_7,      	move,        { .i = 6 } },
+	{ MODKEY,               XK_8,      	move,        { .i = 7 } },
+	{ MODKEY,               XK_9,      	move,        { .i = 8 } },
+	{ MODKEY,               XK_0,      	move,        { .i = 9 } },
 
 	// { MODKEY,               XK_q,      killclient,  { 0 } },
 
